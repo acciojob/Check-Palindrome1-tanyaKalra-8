9539437d60ca018393e273fbd1ba843657ddc928
tanyaKalra-8 <108450417+tanyaKalra-8@users.noreply.github.com>
2023-05-15 17:27:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
-function palindrome(str){
+function palindrome(s){
+	const cleanedString = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
 
+  const length = cleanedString.length;
+  for (let i = 0; i < length / 2; i++) {
+    if (cleanedString[i] !== cleanedString[length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 module.exports = palindrome
