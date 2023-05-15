b4e07fe151881cb387bfd9440f27ea9d4e3558ad
tanyaKalra-8 <108450417+tanyaKalra-8@users.noreply.github.com>
2023-05-15 17:30:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // complete the given function
 
 function palindrome(s){
-	const cleanedString = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+	// const cleanedString = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
 
+	const cleanedString = [];
+	  for (let i = 0; i < s.length; i++) {
+    const char = s[i].toLowerCase();
+    if (isAlphanumeric(char)) {
+      cleanedString.push(char);
+    }
+  }
+	
   const length = cleanedString.length;
   for (let i = 0; i < length / 2; i++) {
     if (cleanedString[i] !== cleanedString[length - 1 - i]) {
